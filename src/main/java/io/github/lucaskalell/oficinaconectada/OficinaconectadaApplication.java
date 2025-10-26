53fb258a7ebde0ad91bbf85cc05ad22367ab1db4
lucas kalell <kalell306@gmail.com>
2025-10-25 21:32:48 -0300

    iniciando projeto

---
+++ src/main/java/io/github/lucaskalell/oficinaconectada/OficinaconectadaApplication.java
@@ -0,0 +1,13 @@
+package io.github.lucaskalell.oficinaconectada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficinaconectadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficinaconectadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/lucaskalell/oficinaconectada/OficinaconectadaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lucaskalell.oficinaconectada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficinaconectadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
